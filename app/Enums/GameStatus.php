b947fe3bf0969c26524d34a0cae0c35c3276c1be
Oleksii-T <alex.tarbeev@gmail.com>
2024-10-04 20:57:51 +0300

    formatters and dummy code

---
+++ app/Enums/GameStatus.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Enums;
+
+use App\Traits\EnumTrait;
+
+enum GameStatus:int
+{
+    use EnumTrait;
+
+    case DRAFT = 1;
+    case PUBLISHED = 2;
+    case CALENDAR_PUBLISHED = 3;
+    case CALENDAR_DRAFT = 4;
+
+    public static function getReadable($val)
+    {
+        return match ($val) {
+            self::DRAFT => 'Draft',
+            self::PUBLISHED => 'Published',
+            self::CALENDAR_DRAFT => 'Calendar Draft',
+            self::CALENDAR_PUBLISHED => 'Calendar Published',
+        };
+    }
+
+}
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
+
 abstract class Controller
 {
-    //
+    /**
+     * Format of success json response for all ajax\axios requests
+     *
+     */
+    public function jsonSuccess($msg='', $data=null): JsonResponse
+    {
+        $resp = [
+            'success' => true,
+            'data' => $data,
+            'message' => $msg
+        ];
+        return response()->json($resp);
+    }
+
+    /**
+     * Format of error json response for all ajax\axios requests
+     *
+     */
+    public function jsonError($msg='Server Error', $data=null, $code=500): JsonResponse
+    {
+        if ($code == 422) {
+            return response()->json(['errors' => $data], $code);
+        }
+        $res = [
+            'success' => false,
+            'data' => $data,
+            'message' => $msg
+        ];
+        return response()->json($res, $code);
+    }
 }
